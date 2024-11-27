bf0576408e3a5fc7ea1c24eb1836b84ffe7abcd6
Kands221 <andeskyle22@gmail.com>
2024-11-27 18:49:43 +0800

    cors

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
     formData.append('file', file);
 
     try {
-      const response = await axios.post(process.env.REACT_APP_API_URL + '/upload', formData, {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/upload`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
